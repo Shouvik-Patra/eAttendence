22f942b8dac287d4f4bb692bd9ca1dd45a1f5cfa
Shouvik Patra <108801627+Shouvik-Patra@users.noreply.github.com>
2025-09-01 08:18:21 +0530

    Save

--- src/utils/helpers/constants.js
+++ src/utils/helpers/constants.js
@@ -2,7 +2,7 @@ export default {
   BASE_URL: 'https://suda.aswaas.org/v1/api',//----LIVE
   // BASE_URL: 'http://43.205.34.89:8001/v1/api',//----DEV
   timeout: 10000,
-  APP_VERSION: '1.4',
+  APP_VERSION: '1.5',
   TOKEN: 'TOKEN',
   LANGUAGE: 'en',
   ROLE: 'ROLE',
