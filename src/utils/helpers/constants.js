c5ac5a295591cb3b6549747561d4828c3393d97e
Shouvik Patra <108801627+Shouvik-Patra@users.noreply.github.com>
2025-11-19 18:23:06 +0530

    Save new changes

--- src/utils/helpers/constants.js
+++ src/utils/helpers/constants.js
@@ -1,6 +1,6 @@
 export default {
-  // BASE_URL: 'https://suda.aswaas.org/v1/api',//----LIVE
-  BASE_URL: 'https://uncontagious-skylike-tiana.ngrok-free.dev/v1/api',//----DEV
+  BASE_URL: 'https://suda.aswaas.org/v1/api',//----LIVE
+  // BASE_URL: 'https://uncontagious-skylike-tiana.ngrok-free.dev/v1/api',//----DEV
   timeout: 10000,
   APP_VERSION: '1.6',
   TOKEN: 'TOKEN',
