4dfb05dc3a9d4b9e7c8b4609f872b04557b7f848
Shouvik Patra <108801627+Shouvik-Patra@users.noreply.github.com>
2026-07-08 18:15:48 +0530

    Developer mode blocked

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-/**
- * @format
- */
+// remove code ----[remove comment out code]
+
+if (!__DEV__) {
+  console.log = () => {};
+  console.warn = () => {};
+  console.error = () => {};
+}
+
+if (__DEV__) {
+  ErrorUtils.setGlobalHandler(() =>
+    Alert.alert(
+      'Security Restriction',
+      'This application cannot run while Developer Options are enabled.\n\nPlease disable Developer Options from your device settings and launch the application again.',
+      [
+        {
+          text: 'Close',
+          onPress: () => BackHandler.exitApp(),
+        },
+      ],
+      { cancelable: false },
+    ),
+  );
+  throw new Error('App will not run in development mode');
+}
+
 import React from 'react';
-import { AppRegistry, LogBox } from 'react-native';
+import { Alert, AppRegistry, LogBox } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 import Store from './src/redux/Store';
